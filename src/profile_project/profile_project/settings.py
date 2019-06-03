3c9fa61981ef6ac841c7ae019268688e3f31b4c0
mh314k <mh314k@gmail.com>
2019-06-04 04:27:24 +0430

    added user userprofile and migrations

--- src/profile_project/profile_project/settings.py
+++ src/profile_project/profile_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
-    'restframework.authtoken',
+    'rest_framework',
+    'rest_framework.authtoken',
     'profile_api',
 ]
 
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEl = "profile_api.UserProfile"
+#AUTH_USER_MODEL = 'profiles_api.UserProfile'
