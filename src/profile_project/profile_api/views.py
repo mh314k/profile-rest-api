9cd03cf3ad7e7e366a15d60fea9b63f5eb52210e
mh314k <mh314k@gmail.com>
2019-06-04 18:00:19 +0430

    crate first api

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+class HelloApiView(APIView):
+     """docstring"""
+     def get(self, request, format=None):
+         '''docstring'''
+         an_api = [
+            'محمد',
+            'حسین',
+            'خانی',
+            'کوثر',
+            'خیزی'
+         ]
+         return Response({'message':"hello",
+            'an_api':an_api
+         })
