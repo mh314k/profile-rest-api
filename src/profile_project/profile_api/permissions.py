8d796abd224f7adc1eb418e7207ee61e676a7417
mh314k <mh314k@gmail.com>
2019-06-05 01:51:39 +0430

    add user auth and permissions

---
+++ src/profile_project/profile_api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+    '''doc'''
+
+    def has_object_permission(self, request, view, obj):
+        '''doc'''
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+        
--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from . import serializers
+from . import permissions
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -101,3 +103,5 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class=serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes =(permissions.UpdateOwnProfile,)
